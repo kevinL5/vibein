f2915eeb9056d753c8815451c0a142aae71ebdc7
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-10-05 14:51:45 +0200

    Try to solve delete when deploy

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,10 +1,11 @@
 class MusicsController < ApplicationController
-  before_action :authenticate_user!
 
-  def destroy
-    @music = current_user.musics.find(params[:id])
-    @music.destroy
-    redirect_to sources_path
-  end
+ before_action :authenticate_user!
+
+ def destroy
+   @music = current_user.musics.find(params[:id])
+   @music.destroy
+   redirect_to sources_path
+ end
 
 end
