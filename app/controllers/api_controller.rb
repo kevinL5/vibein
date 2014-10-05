ce18e7b04489f65147db2b16a4e85740a1af9c0d
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-10-05 17:17:34 +0200

    Try to find a solition to the -delete problem- when deploy

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,19 +5,14 @@ class ApiController < ApplicationController
     @source = Source.new
     @url = params[:url]
 
-    create
-
-    @source = Source.last
-  end
-
-  def create
     if @url[/^(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})/]
       create_youtube
     elsif @url[/^https?:\/\/(soundcloud.com|snd.sc)\/(.*)$/]
       create_soundcloud
     end
-  end
 
+    @source = Source.last
+  end
 
   private
 
