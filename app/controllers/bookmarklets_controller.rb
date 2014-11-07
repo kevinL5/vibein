1fab578cd8c015acb03651edd023d4f57c9b6b22
Kevin LC <kevin.lemairecolombel@gmail.com>
2014-11-07 17:26:08 +0100

    English first page

--- app/controllers/bookmarklets_controller.rb
+++ app/controllers/bookmarklets_controller.rb
@@ -5,5 +5,25 @@ class BookmarkletsController < ApplicationController
     @user = current_user
     @source = Source.new
     @category = Category.new
+
+    show_friends
+  end
+
+  private
+
+  def show_friends
+    @friends = @user.friends
+
+    if @user.provider == 'facebook'
+      graph = Koala::Facebook::API.new(@user.token)
+      @fb_friends = graph.get_connections("me", "friends")
+
+      @fb_friends.each do |fb_friend|
+        if @friends.where(:friend_uid => fb_friend["id"]).first == nil
+          Friend.create(friend_uid: fb_friend["id"], user_id: @user.id.to_i)
+        end
+      end
     end
+  end
+
 end
