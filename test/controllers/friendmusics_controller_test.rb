a84330ae76e66db5b2e043ea97488e18f612cc43
Kevin LC <kevin.lemairecolombel@gmail.com>
2014-11-09 23:58:00 +0100

    Dry a bit - new addbutton in friend/show

--- test/controllers/friendmusics_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FriendmusicsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/friendmusics_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FriendmusicsHelperTest < ActionView::TestCase
-end
