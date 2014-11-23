27f31e2bfcde8f457718be1c35873b3f4184d4c3
reedkevin88 <reedkevin88@gmail.com>
2014-11-23 05:27:49 +0400

    Modify server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{172.31.40.194}
-role :web, %w{172.31.40.194}
-role :db,  %w{172.31.40.194}
+role :app, %w{54.171.240.238}
+role :web, %w{54.171.240.238}
+role :db,  %w{54.171.240.238}
 
 set :stage, :production
 set :keep_releases, 1
@@ -16,4 +16,4 @@ set :keep_releases, 1
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '172.31.40.194', user: 'deploy', roles: %w{web app}, password: 'Qkflgkrtod'
+server '54.171.240.238', user: 'deploy', roles: %w{web app}, password: 'Qkflgkrtod'
